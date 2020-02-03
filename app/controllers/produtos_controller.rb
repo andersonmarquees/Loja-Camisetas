265b14d78116b992325315da7e30e3cea6c80e30
Anderson <anderson@localhost.localdomain>
2020-02-03 15:50:35 -0300

    Criando o primeiro acesso ao app

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,5 +1,11 @@
 class ProdutosController < ApplicationController
     def index
-        
+        @produtos_por_nome = Produto.all.order(:nome).limit 5
+        @produtos_por_preco = Produto.all.order(:preco).limit 2
+    end
+
+    def create
+        valores = params.require(:produto).permit!
+        produto = Produto.create valores
     end
 end
