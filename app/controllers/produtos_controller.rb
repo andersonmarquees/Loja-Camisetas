6ee248f000d47601e2e6dce4792ffbc27f81f4f9
Anderson <anderson@localhost.localdomain>
2020-02-05 19:18:23 -0300

    Criando as validações

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,7 +1,11 @@
 class ProdutosController < ApplicationController
     def index
-        @produtos_por_nome = Produto.all.order(:nome).limit 5
+        @produtos_por_nome = Produto.all.order(:nome).limit 10
         @produtos_por_preco = Produto.all.order(:preco).limit 2
+    end
+
+    def new
+        @produto = Produto.new
     end
 
     def busca
@@ -10,9 +14,14 @@ class ProdutosController < ApplicationController
     end
 
     def create
-        valores = params.require(:produto).permit!
-        produto = Produto.create valores
-        redirect_to root_url
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+        @produto = Produto.new valores
+        if @produto.save
+            flash[:notice] = "Produto salvo com sucesso"
+            redirect_to root_url
+        else
+            render :new
+        end
     end
 
     def destroy
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,4 @@
 class Produto < ApplicationRecord
+    validates :quantidade, :preco, :nome, :descricao, presence: true
+    validates :nome, length: { minimum: 5}
 end
